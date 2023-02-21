1e96ebe0ba92eadc1f30bd551ef2620b8b622863
Alex Junior <alexjr2oficial@gmail.com>
2023-02-21 10:47:37 -0300

    changing the way of generating notes

--- src/js/generateNotes.js
+++ src/js/generateNotes.js
@@ -9,32 +9,42 @@ const generateNote = () => {
 
     // gerando notas para cada item no array
     notes.forEach((note) => {
-        containerNotes.innerHTML += `
-        <div class="noteCard">
-            <div>
-                <h2 class="titleCard">${note.title}</h2>
+        let divNoteCard = document.createElement("div");
+        divNoteCard.classList.add("noteCard");
 
-                <button id="deleteNote">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        class="bi bi-x-lg"
-                        viewBox="0 0 16 16"
-                    >
-                        <path
-                            d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"
-                        />
-                    </svg>
-                </button>
-            </div
+        let div = document.createElement("div");
 
-            <p>
-                ${note.content}
-            </p>
-        </div>
+        let h2Title = document.createElement("h2");
+        h2Title.classList.add("titleCard");
+        h2Title.innerHTML = `${note.title}`;
+
+        let buttonDelete = document.createElement("button");
+        buttonDelete.classList.add("deleteNote");
+        buttonDelete.innerHTML = `
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-x-lg"
+            viewBox="0 0 16 16"
+        >
+            <path
+                d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"
+            />
+        </svg>
         `;
+
+        div.appendChild(h2Title);
+        div.appendChild(buttonDelete);
+
+        let p = document.createElement("p");
+        p.innerHTML = `${note.content}`;
+
+        divNoteCard.appendChild(div);
+        divNoteCard.appendChild(p);
+
+        containerNotes.appendChild(divNoteCard);
     });
 };
 
