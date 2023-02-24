9c1b00c8b37db8e14eadd97a758b1e4db7abb113
Alex Junior <alexjr2oficial@gmail.com>
2023-02-24 10:29:12 -0300

    Stylizing the notes

--- src/js/generateNotes.js
+++ src/js/generateNotes.js
@@ -25,8 +25,8 @@ const generateNote = () => {
         buttonDelete.innerHTML = `
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
+            width="20"
+            height="20"
             fill="currentColor"
             class="bi bi-x-lg"
             viewBox="0 0 16 16"
